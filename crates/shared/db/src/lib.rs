3dfd3aa0d5adf8ecfc3bcd99cedb2cadf7af2745
MrBlackGhostt <hemantkumar4213@icloud.com>
2026-04-07 13:21:21 +0530

    feat: implement create_user function and successfully test database insertion

--- crates/shared/db/src/lib.rs
+++ crates/shared/db/src/lib.rs
@@ -7,6 +7,18 @@ pub async fn create_connection_pool(database_url: &str) -> Result<PgPool, sqlx::
         .await
 }
 
+pub async fn create_user(pool: &PgPool, id: i64, email: &str) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO users (id, email) VALUES ($1, $2)"#,
+        id,
+        email
+    )
+    .execute(pool)
+    .await?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,5 +38,16 @@ mod tests {
             .expect("Failed to execute Select 1");
         assert_eq!(row.0, 1);
         println!("Database connection successful");
+    }
+
+    #[tokio::test]
+    async fn test_create_user() {
+        dotenvy::dotenv().ok();
+        let database_url = var("DATABASE_URL").expect("CRITICAL DATABASE_URL must be set in .env or system");
+        let pool = create_connection_pool(&database_url).await.expect("Pool connection failed");
+        let user_id = 1;
+        let email = "[EMAIL_ADDRESS]";
+        create_user(&pool, user_id, email).await.expect("Failed to create user");
+        println!("User created successfully");
     }
 }
